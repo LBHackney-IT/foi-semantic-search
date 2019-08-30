706c884d76a10e31d972cb193e6262246e4cf203
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-08-30 13:56:01 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+data_path = '../infreemation-data/'
+model_path = 'models/'
+preprocessed_filename = 'infreemation-preprocessed.pickle'
+minimal_df_filename = 'df_min.pickle'
+sentence_embeddings_lookup_filename = 'sentence_embeddings_lookup.pickle'
+word_model_filename = 'word2vec.model'
+word_model_filepath = model_path + word_model_filename
