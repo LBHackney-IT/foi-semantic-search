f4a76d6dee70b5a0019bef6b74893511fe8b7d0b
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-09-05 13:35:54 +0100

    more informative filename

--- sentence_lookup.py
+++ sentence_lookup.py
@@ -11,14 +11,15 @@ base_path = ''
 filepath = config.data_path + config.preprocessed_filename
 df = pd.read_pickle(filepath)
 
+df['request_preview'] = df.apply(lambda x: functions.generate_request_preview(x['requestbody'], 25), axis=1)
+
 # Keep only what we need from the dataframe
-df = df[['subject','url','subject_prepared','id']]
+df = df[['subject','url','subject_prepared','id', 'request_preview']]
 
 # Generate sentence embeddings
 df['subject_embedding'] = df.apply(lambda x: functions.sent2vec(sentence=x['subject_prepared'],model=model_word), axis=1)
 
 # Store the dataframe
-filename = config.minimal_df_filename
+filename = config.subjects_lookup_df_filename
 filepath = base_path + filename
 df.reset_index(drop=True).to_pickle(filepath)
-
