0b1aba4c9a290b5dea25f7e49c5bf330ab35d2fb
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-09-26 11:21:26 +0100

    add request body to preprocessing

--- infreemation_preprocessing.py
+++ infreemation_preprocessing.py
@@ -1,10 +1,16 @@
 import json
 import pandas as pd
 from pandas.io.json import json_normalize
+from nltk.tokenize import sent_tokenize
 import nltk
 import functions
 import config
 
+def prepare_requestbody(s):
+  l = sent_tokenize(s)
+  l = [functions.prepare_text(e) for e in l]
+  l = [e for e in l if e != '']
+  return l
 
 # Read json files from infreemation reporting API, new ones added
 # periodically
@@ -25,16 +31,19 @@ for i in range(1,6):
 for i in df.index:
   df.at[i, 'id'] = functions.extract_id(df.iloc[i]['url'])
 
-# Strip HTML from requestbody
-for i in df.index:
-    df.at[i, 'requestbody_stripped'] = functions.strip_element(s = df.iloc[i]['requestbody'])
-
 # Prepare subject field, which is plain text
 for i in df.index:
     try:
-        df.at[i, 'subject_prepared'] = prepare_text(df.iloc[i]['subject'])
+        df.at[i, 'subject_prepared'] = functions.prepare_text(df.iloc[i]['subject'])
     except:
         print(df.iloc[i]['subject'])
+
+# Prepare request body
+# Strip HTML
+for i in df.index:
+    df.at[i, 'requestbody_stripped'] = functions.strip_element(s = df.iloc[i]['requestbody'])
+# Remove stopwords, non alpha, etc.
+df['requestbody_prepared'] = df.apply(lambda x: prepare_requestbody(x['requestbody_stripped']), axis=1)
 
 # Store pre-processed data
 filename = config.preprocessed_filename
