f6349689c82bbf47385aa0a123261fce6aebb8ce
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-09-26 11:17:44 +0100

    add cols to display in search results

--- generate_sentence_lookup.py
+++ generate_sentence_lookup.py
@@ -11,7 +11,8 @@ df = pd.read_pickle(config.preprocessed_filepath)
 df['request_preview'] = df.apply(lambda x: functions.generate_request_preview(x['requestbody'], 25), axis=1)
 
 # Keep only what we need from the dataframe
-df = df[['subject','url','subject_prepared','id', 'request_preview']]
+df = df[['subject','url','subject_prepared', 'requestbody_prepared', 'id', 'request_preview']]
+
 
 # Generate sentence embeddings
 df['sentence_embedding'] = df.apply(lambda x: functions.sent2vec(sentence=x['subject_requestbody'],model=model_word), axis=1)
