12fd145e7b3133788f099804ab3712791c5a6175
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-09-26 11:16:01 +0100

    tidy file paths

--- generate_sentence_lookup.py
+++ generate_sentence_lookup.py
@@ -6,10 +6,7 @@ import gensim
 # Word2vec model
 model_word = gensim.models.Word2Vec.load(config.word_model_filepath)
 
-base_path = ''
-
-filepath = config.data_path + config.preprocessed_filename
-df = pd.read_pickle(filepath)
+df = pd.read_pickle(config.preprocessed_filepath)
 
 df['request_preview'] = df.apply(lambda x: functions.generate_request_preview(x['requestbody'], 25), axis=1)
 
@@ -20,6 +17,4 @@ df = df[['subject','url','subject_prepared','id', 'request_preview']]
 df['subject_embedding'] = df.apply(lambda x: functions.sent2vec(sentence=x['subject_prepared'],model=model_word), axis=1)
 
 # Store the dataframe
-filename = config.subjects_lookup_df_filename
-filepath = base_path + filename
-df.reset_index(drop=True).to_pickle(filepath)
+df.reset_index(drop=True).to_pickle(config.search_lookup_filepath)
