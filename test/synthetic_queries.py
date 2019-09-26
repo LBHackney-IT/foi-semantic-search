e36bc787d4eed29381d82a142a9ee0263c46e262
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-09-26 11:23:19 +0100

    consistent quoting

--- test/synthetic_queries.py
+++ test/synthetic_queries.py
@@ -2,6 +2,6 @@
 # Some queries are paraphrased from existing request subject lines.
 
 queries = [
-    ("beach huts", '7684'), # "Beach Huts and Play Space"
-    ("catering expenditure", '1510'),  # "What is the cost of biscuits at all councilor meetings in 2017?"
+    ('beach huts', '7684'), # "Beach Huts and Play Space"
+    ('catering expenditure', '1510'),  # "What is the cost of biscuits at all councilor meetings in 2017?"
 ]
