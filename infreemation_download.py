24ef7cf4558b40aa16bdce0ba39d10849ad916f6
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-10-11 11:19:39 +0100

    check for latest date, download, write files

--- infreemation_download.py
+++ infreemation_download.py
@@ -3,14 +3,54 @@ import json
 import config
 from dotenv import load_dotenv
 import os
+import pandas as pd
+from datetime import date, datetime as dt
+from shutil import copyfile
 
+# ----------------------------------------------------------------------
 # Download new entries in the FOI disclosure log using the Infreemation
 # reporting API. This needs to be run via a whitelisted IP address.
-# The API seems to want the params passed in both the URL query string
-# and the posted payload.
 # Expected date format is YYYY-MM-DD
+# ----------------------------------------------------------------------
 
+# Get our preprocessed requests
+df = pd.read_pickle(config.preprocessed_filepath)
+# Get date of latest request we already have
+latest_date = df['datepublished'].sort_values(ascending=False).iloc[0]
+
+today_date = dt.strftime(date.today(), '%Y-%m-%d')
+
+# Get secrets for API
 load_dotenv()
 INFREEMATION_USERNAME = os.getenv('INFREEMATION_USERNAME')
 INFREEMATION_KEY = os.getenv('INFREEMATION_KEY')
 
+# Ask for published requests from that date until today. The API seems
+# to want the params passed in both the URL query string and the posted
+# payload.
+url = f'https://api.infreemation.co.uk/live/foi/?rt=PUBLISHED&key={INFREEMATION_KEY}&username={INFREEMATION_USERNAME}&startdate={latest_date}&enddate={today_date}&status=all'
+
+body = {
+    "rt": "PUBLISHED",
+    "key": INFREEMATION_KEY,
+    "username": INFREEMATION_USERNAME,
+    "startdate": latest_date,
+    "enddate": today_date,
+    "status": "all",
+}
+
+response = requests.post(url, data=json.dumps(body))
+
+# Write an archive version of the file to be used when model training
+# from scratch
+filename = f'infreemation-download-{latest_date}-to-{today_date}-retrieved-on-{today_date}.json'
+archive_filepath = config.data_path + filename
+print('writing ' + archive_filepath)
+with open(archive_filepath, 'wb') as f:
+    f.write(response.content)
+
+# Make the copy that gets picked up when updating the model and the
+# search lookup
+latest_filepath = config.data_path + 'latest.json'
+print('copying to ' + latest_filepath)
+copyfile(archive_filepath, latest_filepath)
