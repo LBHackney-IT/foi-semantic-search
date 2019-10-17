1e027a85ae925f307cb3992e08228669a67551b2
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-10-17 15:57:57 +0100

    Add main()

--- dash-preprocessing.py
+++ dash-preprocessing.py
@@ -7,20 +7,28 @@ import config
 # consumes less memory -- these factors are important for deployment on
 # App Engine
 
-# Word2vec model
-model_word = gensim.models.Word2Vec.load(config.word_model_filepath)
-vocab = list(model_word.wv.vocab)
 
-# Reduce vector dimensions for plotting
-X = model_word[vocab]
-tsne = TSNE(n_components=2)
-X_tsne = tsne.fit_transform(X)
-df = pd.DataFrame(X_tsne, index=vocab, columns=['x', 'y'])
-df = df.reset_index()
-df.columns = ['vocab', 'x', 'y']
-df = df.sort_values(by=['vocab'])
+def main():
+    # Word2vec model
+    model_word = gensim.models.Word2Vec.load(config.word_model_filepath)
+    vocab = list(model_word.wv.vocab)
 
-# Cache most similar words for all words in vocab
-df['most_similar'] = df.apply(lambda x: model_word.wv.most_similar(x['vocab']), axis=1)
+    # Reduce vector dimensions for plotting
+    X = model_word[vocab]
+    tsne = TSNE(n_components=2)
+    X_tsne = tsne.fit_transform(X)
+    df = pd.DataFrame(X_tsne, index=vocab, columns=['x', 'y'])
+    df = df.reset_index()
+    df.columns = ['vocab', 'x', 'y']
+    df = df.sort_values(by=['vocab'])
 
-df.to_pickle(config.viz_df_filepath)
+    # Cache most similar words for all words in vocab
+    df['most_similar'] = df.apply(
+        lambda x: model_word.wv.most_similar(x['vocab']), axis=1
+    )
+
+    df.to_pickle(config.viz_df_filepath)
+
+
+if __name__ == "__main__":
+    main()
