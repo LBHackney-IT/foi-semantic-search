b9c0227e2b354bb43cb4bf2972e3b8cb894769d5
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-10-17 16:00:57 +0100

    Add main()

--- build-tfidf-model.py
+++ build-tfidf-model.py
@@ -2,10 +2,14 @@ import config
 import gensim
 import pickle
 
-model = gensim.models.Word2Vec.load(config.word_model_filepath)
-dictionary = gensim.corpora.Dictionary([list(model.wv.vocab.keys())])
-with open(config.tokenized_filepath, 'rb') as f:
-    tokenized = pickle.load(f)
-corpus = [dictionary.doc2bow(text) for text in tokenized]
-tfidf = gensim.models.TfidfModel(corpus, id2word=dictionary)
-tfidf.save(config.tfidf_filepath)
+def main():
+    model = gensim.models.Word2Vec.load(config.word_model_filepath)
+    dictionary = gensim.corpora.Dictionary([list(model.wv.vocab.keys())])
+    with open(config.tokenized_filepath, 'rb') as f:
+        tokenized = pickle.load(f)
+    corpus = [dictionary.doc2bow(text) for text in tokenized]
+    tfidf = gensim.models.TfidfModel(corpus, id2word=dictionary)
+    tfidf.save(config.tfidf_filepath)
+
+if __name__ == "__main__":
+    main()
