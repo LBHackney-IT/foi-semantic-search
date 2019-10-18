423d72ca8aa5909fa578d029d7da63a9fac4126d
tmpks <36076608+LBHTPerkins@users.noreply.github.com>
2019-10-18 16:30:00 +0100

    rename file

--- test/synthetic_queries.py
+++
@@ -1,9 +0,0 @@
-# Queries, and the IDs of corresponding requests that are expected to appear in the top n results.
-# Some queries are paraphrased from existing request subject lines.
-
-queries = [
-    # "Beach Huts and Play Space"
-    ('beach huts', '7684'),
-    # "What is the cost of biscuits at all councilor meetings in 2017?"
-    ('catering expenditure', '1510'),
-]
